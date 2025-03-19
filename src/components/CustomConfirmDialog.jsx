447f634a2e069484ba8dc101c05e5f3939db1b43
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2025-03-19 11:54:50 +0700

    add CustomConfirmDialog component

---
+++ src/components/CustomConfirmDialog.jsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { useTheme } from '@mui/material/styles';
+import { tokens } from '../theme';
+
+const CustomConfirmDialog = ({ open, title, question, onClose }) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+    const handleClose = (response) => {
+        onClose(response);
+    };
+
+    return (
+        <Dialog
+            open={open}
+            onClose={() => handleClose(false)}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+            maxWidth="md" // Set the maximum width of the dialog
+            fullWidth // Make the dialog take the full width of the screen
+            sx={{
+                '& .MuiDialog-paper': {
+                    minWidth: '500px', // Set the minimum width of the dialog
+                    maxWidth: '700px', // Set the maximum width of the dialog
+                },
+            }}
+        >
+            <DialogTitle id="alert-dialog-title" sx={{ fontSize: '1.2rem', color: colors.greenAccent[500] }}>
+                {title}
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-description" sx={{ fontSize: '1.2rem' }}>
+                    {question}
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button sx={{ fontSize: '1.1rem', color: colors.red[500], textTransform: 'none' }} onClick={() => handleClose(false)}>Hủy</Button>
+                <Button sx={{ fontSize: '1.1rem', color: colors.greenAccent[500], textTransform: 'none' }} onClick={() => handleClose(true)} autoFocus>
+                    Xác nhận
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+export default CustomConfirmDialog;
