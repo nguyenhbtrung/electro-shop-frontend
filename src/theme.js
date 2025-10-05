2695325149d0b27b563399afe1d27d8f16bd56f4
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2025-10-05 13:11:12 +0700

    save theme in localStorage

--- src/theme.js
+++ src/theme.js
@@ -274,11 +274,13 @@ export const ColorModeContext = createContext({
 });
 
 export const useMode = () => {
-  const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState(localStorage.getItem("theme") || "light");
 
   const colorMode = useMemo(() => ({
-    toggleColorMode: () =>
-      setMode((prev) => (prev === "light" ? "dark" : "light")),
+    toggleColorMode: () => {
+      localStorage.setItem("theme", mode === "light" ? "dark" : "light");
+      setMode((prev) => (prev === "light" ? "dark" : "light"))
+    },
   }));
 
   const theme = useMemo(() => createTheme(themeSettings(mode), [mode]));
