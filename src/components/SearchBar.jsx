f36ab91071f20629ef76f7ac5fae79f330a6d565
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2026-06-25 19:17:39 +0700

    refactor: simplify SearchBar component by removing unused media query and updating submit handling

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ import InputBase from "@mui/material/InputBase";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import SearchOutlined from "@mui/icons-material/SearchOutlined";
-import { useMediaQuery, useTheme } from "@mui/material";
+import { useTheme } from "@mui/material";
 import { tokens } from "../theme";
 import { useState } from "react";
 
@@ -10,18 +10,23 @@ const SearchBar = ({ onSearch }) => {
     const [searchTerm, setSearchTerm] = useState("");
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
-    const isXsDevices = useMediaQuery("(max-width:466px)");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSearch(searchTerm);
+    };
 
     return (
         <Box
+            component="form"
+            onSubmit={handleSubmit}
             display="flex"
             alignItems="center"
             bgcolor={colors.primary[400]}
             borderRadius="3px"
             sx={{
-                display: "flex",
-                width: {xs: 350, md: 400, xl: 550},
-                flex: 1
+                width: { xs: 350, md: 400, xl: 550 },
+                flex: 1,
             }}
         >
             <InputBase
@@ -30,7 +35,8 @@ const SearchBar = ({ onSearch }) => {
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
             />
-            <IconButton type="button" sx={{ p: 1 }} onClick={() => onSearch(searchTerm)}>
+
+            <IconButton type="submit" sx={{ p: 1 }}>
                 <SearchOutlined />
             </IconButton>
         </Box>
